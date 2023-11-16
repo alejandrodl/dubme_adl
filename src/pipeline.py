9ac098e21b6e5a62374732aba62750da02d4f97f
alejandrodl <adelgalu@gmail.com>
2023-11-16 15:17:55 +0100

    improve readability

--- src/pipeline.py
+++ src/pipeline.py
@@ -14,66 +14,69 @@ from utils import (
 
 
 # Separation models
-model = "FAIR"  # 'FAIR' or 'CleanUNet'
-print("Using {} model".format(model))
+MODEL = "FAIR"  # 'FAIR' or 'CleanUNet'
+print("Using {} model".format(MODEL))
 
 # Set extensions
-video_ext = ".mp4"
-audio_ext = ".wav"
-text_ext = ".txt"
+VIDEO_EXT = ".mp4"
+AUDIO_EXT = ".wav"
+TEXT_EXT = ".txt"
 
 # Set paths
-video_directory = "data/videos"
-audio_directory = "data/audios"
-denoised_audio_directory = "data/denoised"
-noise_audio_directory = "data/noise"
-transcriptions_directory = "data/transcriptions"
+VIDEO_DIRECTORY = "data/videos"
+AUDIO_DIRECTORY = "data/audios"
+DENOISED_AUDIO_DIRECTORY = "data/denoised"
+NOISE_AUDIO_DIRECTORY = "data/noise"
+TRANSCRIPTIONS_DIRECTORY = "data/transcriptions"
 
 # Create audio directories if they don't exist
-clear_make_directory(audio_directory, audio_ext)
-clear_make_directory(denoised_audio_directory, audio_ext)
-clear_make_directory(noise_audio_directory, audio_ext)
-clear_make_directory(transcriptions_directory, text_ext)
+clear_make_directory(AUDIO_DIRECTORY, AUDIO_EXT)
+clear_make_directory(DENOISED_AUDIO_DIRECTORY, AUDIO_EXT)
+clear_make_directory(NOISE_AUDIO_DIRECTORY, AUDIO_EXT)
+clear_make_directory(TRANSCRIPTIONS_DIRECTORY, TEXT_EXT)
 
 # STEP 1 - Get video paths and extract audio from each video
 video_paths = [
-    os.path.join(video_directory, filename)
-    for filename in os.listdir(video_directory)
-    if filename.endswith(video_ext)
+    os.path.join(VIDEO_DIRECTORY, filename)
+    for filename in os.listdir(VIDEO_DIRECTORY)
+    if filename.endswith(VIDEO_EXT)
 ]
+
 for video_path in video_paths:
-    extract_audio_from_video(video_path, audio_directory)
+    extract_audio_from_video(video_path, AUDIO_DIRECTORY)
 
 # STEP 2 - Convert all audio files to 16kHz mono
 audio_paths = [
-    os.path.join(audio_directory, filename)
-    for filename in os.listdir(audio_directory)
-    if filename.endswith(audio_ext)
+    os.path.join(AUDIO_DIRECTORY, filename)
+    for filename in os.listdir(AUDIO_DIRECTORY)
+    if filename.endswith(AUDIO_EXT)
 ]
+
 for audio_path in audio_paths:
     downsample_and_mono_from_path(audio_path)
 
-# STEP 3 - Get audio paths and denoise each audio
+# STEP 3 - Get audio paths, denoise each audio and time it
 start_time = time.time()
 for audio_path in audio_paths:
-    denoise_audio_file(audio_path, denoised_audio_directory, model)
+    denoise_audio_file(audio_path, DENOISED_AUDIO_DIRECTORY, MODEL)
 print("Denoising took {} seconds".format(time.time() - start_time))
 
 # STEP 4 - Get denoised audio paths and subtract them from original audio to get noise
 denoised_audio_paths = [
-    os.path.join(denoised_audio_directory, filename)
-    for filename in os.listdir(denoised_audio_directory)
-    if filename.endswith(audio_ext)
+    os.path.join(DENOISED_AUDIO_DIRECTORY, filename)
+    for filename in os.listdir(DENOISED_AUDIO_DIRECTORY)
+    if filename.endswith(AUDIO_EXT)
 ]
+
 for audio_path in audio_paths:
-    denoised_audio_path = denoised_audio_directory + "/" + audio_path.split("/")[-1]
-    noise_path = noise_audio_directory + "/" + denoised_audio_path.split("/")[-1]
+    denoised_audio_path = DENOISED_AUDIO_DIRECTORY + "/" + audio_path.split("/")[-1]
+    noise_path = NOISE_AUDIO_DIRECTORY + "/" + denoised_audio_path.split("/")[-1]
     get_noise_files(audio_path, denoised_audio_path, noise_path)
 
-# STEP 5 - Transcribe audio files and denoised audio files and save it to transcriptions_directory
+# STEP 5 - Transcribe audio files and denoised audio files and save it to TRANSCRIPTIONS_DIRECTORY
 for audio_path in audio_paths:
     transcriptions_path = (
-        transcriptions_directory
+        TRANSCRIPTIONS_DIRECTORY
         + "/"
         + audio_path.split("/")[-1].split(".")[0]
         + ".txt"
@@ -82,7 +85,7 @@ for audio_path in audio_paths:
 
 for denoised_audio_path in denoised_audio_paths:
     transcriptions_path = (
-        transcriptions_directory
+        TRANSCRIPTIONS_DIRECTORY
         + "/"
         + denoised_audio_path.split("/")[-1].split(".")[0]
         + "_denoised.txt"
@@ -91,13 +94,14 @@ for denoised_audio_path in denoised_audio_paths:
 
 # STEP 6 (Optional) - Compute audio quality metrics and save them to a csv file
 denoised_audio_paths = [
-    os.path.join(denoised_audio_directory, filename)
-    for filename in os.listdir(denoised_audio_directory)
-    if filename.endswith(audio_ext)
+    os.path.join(DENOISED_AUDIO_DIRECTORY, filename)
+    for filename in os.listdir(DENOISED_AUDIO_DIRECTORY)
+    if filename.endswith(AUDIO_EXT)
 ]
+
 audio_metrics_dict: dict = {}
 for denoised_audio_path in denoised_audio_paths:
-    original_audio_path = audio_directory + "/" + denoised_audio_path.split("/")[-1]
+    original_audio_path = AUDIO_DIRECTORY + "/" + denoised_audio_path.split("/")[-1]
     audio_metrics_dict = compute_audio_quality_metrics(
         audio_metrics_dict,
         denoised_audio_path,
@@ -106,7 +110,7 @@ for denoised_audio_path in denoised_audio_paths:
         rel_metrics=["stoi", "sisdr"],
     )
 
-metrics_path = "data/audio_metrics_" + model + ".csv"
+metrics_path = "data/audio_metrics_" + MODEL + ".csv"
 if os.path.exists(metrics_path):
     os.remove(metrics_path)
     save_metrics(audio_metrics_dict, metrics_path)
@@ -117,13 +121,13 @@ else:
 transcription_metrics_dict: dict = {}
 for denoised_audio_path in denoised_audio_paths:
     transcription_gt_path = (
-        transcriptions_directory
+        TRANSCRIPTIONS_DIRECTORY
         + "_gt/"
         + denoised_audio_path.split("/")[-1].split(".")[0]
         + "_gt.txt"
     )
     transcription_path = (
-        transcriptions_directory
+        TRANSCRIPTIONS_DIRECTORY
         + "/"
         + denoised_audio_path.split("/")[-1].split(".")[0]
         + ".txt"
@@ -141,7 +145,7 @@ for denoised_audio_path in denoised_audio_paths:
         transcription_denoised_path,
     )
 
-metrics_path = "data/transcription_metrics_" + model + ".csv"
+metrics_path = "data/transcription_metrics_" + MODEL + ".csv"
 if os.path.exists(metrics_path):
     os.remove(metrics_path)
     save_metrics(transcription_metrics_dict, metrics_path)
